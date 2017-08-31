1e5c62a4593e815519717e2d56cb04b687f25716
Andrea Franceschini <andrea.franceschini@gmail.com>
2017-08-31 22:15:18 +0100

    :frowning: UglifyJS is a mystery to me...

--- webpack.prod.js
+++ webpack.prod.js
@@ -6,7 +6,7 @@ const common = require('./webpack.common.js');
 module.exports = merge(common, {
     devtool: 'cheap-module-source-map',
     plugins: [
-        new UglifyJSPlugin(),
+        // new UglifyJSPlugin(),
         new webpack.DefinePlugin({
             'process.env': {
                 'NODE_ENV': JSON.stringify('production')
